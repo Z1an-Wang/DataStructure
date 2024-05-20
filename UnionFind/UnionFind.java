d076e8286de16eb39a0784677ae727a55068e0b7
Zian Wang <841048736@qq.com>
2024-05-20 18:23:42 +0800

    refactor the Union Find

--- UnionFind/UnionFind.java
+++ UnionFind/UnionFind.java
@@ -1,101 +1,34 @@
 package UnionFind;
-import java.util.Arrays;
 
-public class UnionFind {
-    // The number of elements in this union find
-    private int size;
+public abstract class UnionFind {
 
-    // Used to track the sizes of each components
-    private int[] sz;
+    protected int[] parents;
 
-    // id[i] points to the parent of i, if id[i]=i then i is the root node
-    private int[] id;
+    public UnionFind(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("capacity must be >= 1");
+        }
 
-    // Track the number of components in the union find.
-    private int numComponents;
-
-    public UnionFind(int size) {
-        if (size <= 0)
-            throw new IllegalArgumentException();
-
-        this.size = numComponents = size;
-        sz = new int[size];
-        id = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            id[i] = i; // Link to itself (self root)
-            sz[i] = 1; // Each component is originally for size 1
+        parents = new int[capacity];
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
         }
     }
 
-    // Find which components/sets 'p' belongs to, takes amortized constant time
-    public int find(int p) {
-        // Find the root of the component
-        int root = p;
-        while (root != id[root]) {
-            root = id[root];
-        }
-        // Compress the path leading back to the root - Amortized constant time
-        while (p != root) {
-            int next = id[p];
-            id[p] = root;
-            p = next;
-        }
-        return root;
+    /**
+     * 查找 v 的根节点
+     */
+    public abstract int find(int v);
+
+    /**
+     * 合并 v1、v2 所在的集合
+     */
+    public abstract void union(int v1, int v2);
+
+    /**
+     * 检查v1、v2是否属于同一个集合
+     */
+    public boolean isSame(int v1, int v2) {
+        return find(v1) == find(v2);
     }
-
-    // Return if elements 'p' and 'q' belong to the same component/set
-    public boolean connected(int p, int q) {
-        return find(p) == find(q);
-    }
-
-    // Return the size of the component/set which 'p' belongs to.
-    public int componentSize(int p) {
-        return sz[find(p)];
-    }
-
-    // Return the number of elements in this UF/Disjoint Set
-    public int size() {
-        return size;
-    }
-
-    // Returns the number of remaining components/Sets
-    public int components() {
-        return numComponents;
-    }
-
-    // Unify the components/sets containing elements 'p' and 'q'.
-    public void unify(int p, int q) {
-        int rootp = find(p);
-        int rootq = find(q);
-
-        // Elements 'p' and 'q' are in the same group.
-        if (rootp == rootq) {
-            return;
-        }
-        // Merge two component together (into larger one)
-        if (sz[rootp] < sz[rootq]) {
-            id[rootp] = rootq;
-            sz[rootq] += sz[rootp];
-        } else {
-            id[rootq] = rootp;
-            sz[rootp] += sz[rootq];
-        }
-        // Since we merge one component, reduce 1.
-        numComponents--;
-    }
-
-    // public static void main(String[] args) {
-    //     UnionFind nf = new UnionFind(10);
-    //     nf.unify(1, 2);
-    //     nf.unify(2, 3);
-    //     nf.unify(5, 6);
-    //     nf.unify(6, 7);
-    //     nf.unify(7, 8);
-    //     nf.unify(2, 7);
-    //     System.out.println(Arrays.toString(nf.id));
-    //     System.out.println(Arrays.toString(nf.sz));
-    //     System.out.println(nf.components());
-    //     System.out.println(nf.componentSize(2));
-    // }
 }
---
+++ UnionFind/UnionFind_.java
@@ -0,0 +1,101 @@
+package UnionFind;
+import java.util.Arrays;
+
+public class UnionFind_ {
+    // The number of elements in this union find
+    private int size;
+
+    // Used to track the sizes of each components
+    private int[] sz;
+
+    // id[i] points to the parent of i, if id[i]=i then i is the root node
+    private int[] id;
+
+    // Track the number of components in the union find.
+    private int numComponents;
+
+    public UnionFind_(int size) {
+        if (size <= 0)
+            throw new IllegalArgumentException();
+
+        this.size = numComponents = size;
+        sz = new int[size];
+        id = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            id[i] = i; // Link to itself (self root)
+            sz[i] = 1; // Each component is originally for size 1
+        }
+    }
+
+    // Find which components/sets 'p' belongs to, takes amortized constant time
+    public int find(int p) {
+        // Find the root of the component
+        int root = p;
+        while (root != id[root]) {
+            root = id[root];
+        }
+        // Compress the path leading back to the root - Amortized constant time
+        while (p != root) {
+            int next = id[p];
+            id[p] = root;
+            p = next;
+        }
+        return root;
+    }
+
+    // Return if elements 'p' and 'q' belong to the same component/set
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // Return the size of the component/set which 'p' belongs to.
+    public int componentSize(int p) {
+        return sz[find(p)];
+    }
+
+    // Return the number of elements in this UF/Disjoint Set
+    public int size() {
+        return size;
+    }
+
+    // Returns the number of remaining components/Sets
+    public int components() {
+        return numComponents;
+    }
+
+    // Unify the components/sets containing elements 'p' and 'q'.
+    public void unify(int p, int q) {
+        int rootp = find(p);
+        int rootq = find(q);
+
+        // Elements 'p' and 'q' are in the same group.
+        if (rootp == rootq) {
+            return;
+        }
+        // Merge two component together (into larger one)
+        if (sz[rootp] < sz[rootq]) {
+            id[rootp] = rootq;
+            sz[rootq] += sz[rootp];
+        } else {
+            id[rootq] = rootp;
+            sz[rootp] += sz[rootq];
+        }
+        // Since we merge one component, reduce 1.
+        numComponents--;
+    }
+
+    // public static void main(String[] args) {
+    //     UnionFind nf = new UnionFind(10);
+    //     nf.unify(1, 2);
+    //     nf.unify(2, 3);
+    //     nf.unify(5, 6);
+    //     nf.unify(6, 7);
+    //     nf.unify(7, 8);
+    //     nf.unify(2, 7);
+    //     System.out.println(Arrays.toString(nf.id));
+    //     System.out.println(Arrays.toString(nf.sz));
+    //     System.out.println(nf.components());
+    //     System.out.println(nf.componentSize(2));
+    // }
+}
