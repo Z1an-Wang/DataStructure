042268a1b62eabeba74804194e4658150d3d366a
Zian Wang <841048736@qq.com>
2024-05-21 20:22:51 +0800

    Add Kruskal algorithm to generate MST

--- Graph/MinimumSpanningTree.java
+++ Graph/MinimumSpanningTree.java
@@ -1,32 +1,38 @@
 package Graph;
 
 import java.util.*;
+import UnionFind.GenericUnionFind;
 
 public class MinimumSpanningTree {
 
+    /*********************************************************
+     *          Adjacent Matrix Definition.
+     *********************************************************/
     public static final int[][] _adjMatrix = {
-            { -1, 3, 2, -1, -1, -1, -1, -1, 7, -1 },
-            { 3, -1, 4, 5, -1, -1, -1, -1, -1, -1 },
-            { 2, 4, -1, 2, 9, 6, -1, -1, -1, -1 },
-            { -1, 5, 2, -1, 7, -1, 9, -1, -1, -1 },
-            { -1, -1, 9, 7, -1, 9, -1, -1, -1, -1 },
-            { -1, -1, 6, -1, 9, -1, 8, -1, 9, 2 },
-            { -1, -1, -1, 9, -1, 8, -1, 10, -1, -1 },
-            { -1, -1, -1, -1, -1, -1, 10, -1, 7, -1 },
-            { 7, -1, -1, -1, -1, 9, -1, 7, -1, 10 },
-            { -1, -1, -1, -1, -1, 2, -1, -1, 10, -1 }
+            { -1,  3,  2, -1, -1, -1, -1, -1,  7, -1 },
+            {  3, -1,  4,  5, -1, -1, -1, -1, -1, -1 },
+            {  2,  4, -1,  2,  9,  6, -1, -1, -1, -1 },
+            { -1,  5,  2, -1,  7, -1,  9, -1, -1, -1 },
+            { -1, -1,  9,  7, -1,  9, -1, -1, -1, -1 },
+            { -1, -1,  6, -1,  9, -1,  8, -1,  9,  2 },
+            { -1, -1, -1,  9, -1,  8, -1, 10, -1, -1 },
+            { -1, -1, -1, -1, -1, -1, 10, -1,  7, -1 },
+            {  7, -1, -1, -1, -1,  9, -1,  7, -1, 10 },
+            { -1, -1, -1, -1, -1,  2, -1, -1, 10, -1 }
     };
     public static final int[][] _adjMatrix2 = {
-            {0, 2, 0, 6},
-            {2, 0, 3, 5},
-            {0, 3, 0, 4},
-            {6, 5, 4, 0}
+            { 0, 2, 0, 6 },
+            { 2, 0, 3, 5 },
+            { 0, 3, 0, 4 },
+            { 6, 5, 4, 0 }
     };
 
+
+    /*********************************************************
+     *    Prim algorithm Minimum Spanning Tree Generation
+     *********************************************************/
     /**
-     * Generate Minimum Spanning Tree using Prim algorithm
      * @param adjMatrix Adjacent Matrix MUST be a connected weight graph.
-     * @return
      */
     public static int[][] Prim(int[][] adjMatrix) {
         // initialize the size of graph and result MST.
@@ -93,9 +99,78 @@ public class MinimumSpanningTree {
         return neighbours;
     }
 
+
+    /*********************************************************
+     *  Kruskal algorithm Minimum Spanning Tree Generation
+     *********************************************************/
+    public static int[][] Kruskal(int[][] adjMatrix) {
+        // Initialize the result matrix - the minimun spanning tree.
+        int[][] res = new int[adjMatrix.length][adjMatrix.length];
+        for (int[] i : res) {
+            Arrays.fill(i, 0);
+        }
+
+        // Create the edges list and sort them based on their weight.
+        LinkedList<Edge> edges = new LinkedList<>();
+        for (int i = 0; i < adjMatrix.length; i++) {
+            for (int j = i + 1; j < adjMatrix.length; j++) {
+                if (adjMatrix[i][j] > 0) {
+                    edges.add(new Edge(i, j, adjMatrix[i][j]));
+                }
+            }
+        }
+        Collections.sort(edges);
+
+        // adopt the Union Find structure to merge the edge set.
+        LinkedList<Integer> vertexes = new LinkedList<>();
+        for (int i = 0; i < adjMatrix.length; i++) {
+            vertexes.add(i);
+        }
+        GenericUnionFind<Integer> uf = new GenericUnionFind<>(vertexes);
+        // traverse the edges list from shortest to longest.
+        for (Edge e : edges) {
+            // if all endpoints are joined into one set, MST is constructed.
+            if (uf.getComponent() == 1) {
+                break;
+            }
+            // if the edge's two endpoint are not belong to the same UnionFind set, union them.
+            if (!uf.isConnected(e.start, e.end)) {
+                uf.union(e.start, e.end);
+                res[e.start][e.end] = 1;
+            }
+        }
+
+        return res;
+    }
+
+    private static class Edge implements Comparable<Edge> {
+        int start, end, weight;
+
+        Edge(int s, int e, int w) {
+            this.start = s;
+            this.end = e;
+            this.weight = w;
+        }
+
+        @Override
+        public int compareTo(Edge e) {
+            return Integer.compare(this.weight, e.weight);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + start + end + ")";
+        }
+    }
+
     public static void main(String[] args) {
-        int[][] r = Prim(_adjMatrix);
-        for (int[] i : r) {
+        int[][] p = Prim(_adjMatrix);
+        for (int[] i : p) {
+            System.out.println(Arrays.toString(i));
+        }
+        System.out.println("------------------------------");
+        int[][] k = Kruskal(_adjMatrix);
+        for (int[] i : k) {
             System.out.println(Arrays.toString(i));
         }
     }
